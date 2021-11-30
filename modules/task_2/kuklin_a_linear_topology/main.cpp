cb80889b375f8ed22eff88caef410aca9e0ea36e
Andrey Kuklin <andre.kuk@bk.ru>
2021-11-30 23:52:30 +0300

    Added linear topology, added tests

--- modules/task_2/kuklin_a_linear_topology/main.cpp
+++ modules/task_2/kuklin_a_linear_topology/main.cpp
@@ -3,6 +3,81 @@
 #include "./linear_topology.h"
 #include <gtest-mpi-listener.hpp>
 
+TEST(Parrallel_Operations_MPI, can_create_linear_topology) {
+  ASSERT_NO_THROW(GetlinearTopologyComm(MPI_COMM_WORLD));
+}
+
+TEST(Parrallel_Operations_MPI, can_send_data_in_linear_comm) {
+  MPI_Comm lincom = GetlinearTopologyComm(MPI_COMM_WORLD);
+  int procSize, procRank;
+  MPI_Comm_size(lincom, &procSize);
+  MPI_Comm_rank(lincom, &procRank);
+
+  if (procSize == 1) {
+    ASSERT_TRUE(true);
+  } else {
+    if (procRank == 0) {
+      int dataMessage = 10;
+      MPI_Send(&dataMessage, 1, MPI_INT, procSize - 1, 0, lincom);
+    }
+    if (procRank == procSize - 1) {
+      int dataBuff;
+      MPI_Status stat;
+      MPI_Recv(&dataBuff, 1, MPI_INT, 0, 0, lincom, &stat);
+
+      ASSERT_EQ(dataBuff, 10);
+    }
+  }
+}
+
+TEST(Parrallel_Operations_MPI,
+     count_neighbors_of_first_proc_in_linear_comm_is_correct) {
+  MPI_Comm lincom = GetlinearTopologyComm(MPI_COMM_WORLD);
+  int procSize;
+  MPI_Comm_size(lincom, &procSize);
+
+  if (procSize == 1) {
+    ASSERT_TRUE(true);
+  } else {
+    int neigh_count;
+    MPI_Graph_neighbors_count(lincom, 0, &neigh_count);
+
+    ASSERT_EQ(neigh_count, 1);
+  }
+}
+
+TEST(Parrallel_Operations_MPI,
+     count_neighbors_of_last_proc_in_linear_comm_is_correct) {
+  MPI_Comm lincom = GetlinearTopologyComm(MPI_COMM_WORLD);
+  int procSize;
+  MPI_Comm_size(lincom, &procSize);
+
+  if (procSize == 1) {
+    ASSERT_TRUE(true);
+  } else {
+    int neigh_count;
+    MPI_Graph_neighbors_count(lincom, procSize - 1, &neigh_count);
+
+    ASSERT_EQ(neigh_count, 1);
+  }
+}
+
+TEST(Parrallel_Operations_MPI,
+     count_neighbors_of_middle_proc_in_linear_comm_is_correct) {
+  MPI_Comm lincom = GetlinearTopologyComm(MPI_COMM_WORLD);
+  int procSize;
+  MPI_Comm_size(lincom, &procSize);
+
+  if (procSize > 2) {
+    int neigh_count;
+    MPI_Graph_neighbors_count(lincom, 2, &neigh_count);
+
+    ASSERT_EQ(neigh_count, 2);
+  }
+
+  ASSERT_TRUE(true);
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   MPI_Init(&argc, &argv);
